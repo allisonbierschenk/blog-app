7f9ee1cbc1796702610fdf825db691539e047bc0
Andrew Bayer <arbayer@iu.edu>
2021-03-05 10:00:54 -0500

    sorted

--- client/src/utils/sort.js
+++ client/src/utils/sort.js
@@ -1,1 +1,14 @@
-//put sort functions here
+const compareKeys = (key) => (a, b) => {
+  if (a[key].toLowerCase() < b[key].toLowerCase()) {
+    return -1;
+  }
+  if (a[key].toLowerCase() > b[key].toLowerCase()) {
+    return 1;
+  }
+  return 0;
+};
+
+export const AZTitle = (arr) => arr.sort(compareKeys("title"));
+export const ZATitle = (arr) => arr.sort(compareKeys("title")).reverse();
+export const AZAuthor = (arr) => arr.sort(compareKeys("author"));
+export const ZAAuthor = (arr) => arr.sort(compareKeys("author")).reverse();
